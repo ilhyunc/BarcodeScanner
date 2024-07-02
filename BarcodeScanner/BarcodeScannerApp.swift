e3f6f3bacd5b124e11915dec3a7e6eab713a6001
Ilhyun Cho <whdlfgus92@gmail.com>
2024-07-02 11:10:05 -0400

    Barcode Scanner

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Joe on 2024-06-27.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
